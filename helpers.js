81ebfe04a16e33d654bbcb7e48a9d9c099065029
DS99pr <devteachpl@gmail.com>
2026-02-09 00:51:20 +0100

    Adding repl

---
+++ helpers.js
@@ -0,0 +1,9 @@
+function welcome() {
+    console.log(`
+Wes hal on Hearm spraece. Thes waes getimbrod on thaem geare 2026.   
+`.trim())
+}
+
+export default {
+    welcome
+}
--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 //
 // HEARM PROGRAMMING LANGUAGE (2026)
 //
+
+// IMPORTS
+import readline from 'readline'
+import helpers from './helpers.js'
+
+// INTERFACE
+const rli = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// 
+// REPL
+// 
+
+helpers.welcome();
+function repl() {
+    rli.question('>> ', () => {
+        repl();
+    });
+};
+
+repl()
