f719b21b8a92446af7045aa3c7046413a26d233a
Ds <devteachpl@gmail.com>
2026-02-10 02:04:27 +0100

    Create betrayals.js

---
+++ betrayals.js
@@ -0,0 +1,12 @@
+// PARSER ERRORS
+
+const NO_FUNCTION = "Theres no such command like that";
+const NO_LEFT_PARENTHESIS = "No [ here";
+const NO_RIGHT_PARENTHESIS = "No ] here";
+const NO_TYPE_HERE = "You gotta type scripture/rim here";
+
+export default {
+    NO_FUNCTION, NO_TYPE_HERE,
+    NO_LEFT_PARENTHESIS,
+    NO_RIGHT_PARENTHESIS
+}
